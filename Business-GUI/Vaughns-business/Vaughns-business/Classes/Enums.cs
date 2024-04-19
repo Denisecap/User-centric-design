66fb6f9bcbbad13219f13b25071a56944deaa834
Elijah Hiraka <HIRAE1@student.op.ac.nz>
2024-04-19 20:48:54 +1200

    Added Enums class for position and salary

---
+++ Business-GUI/Vaughns-business/Vaughns-business/Classes/Enums.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vaughns_business.Classes
+{
+    public enum EPosition
+    {
+        OWNER = 0,
+        DRIVER = 1,
+        INTERN = 2,
+        RECEPTIONIST = 3,
+        TOUR_GUIDE = 4
+    }
+    public enum ESalary
+    {
+        OWNER_SALARY = 100000,
+        DRIVER_SALARY = 85000,
+        INTERN_SALARY = 50000,
+        RECEPTIONIST_SALARY = 70000,
+        TOUR_GUIDE_SALARY = 60000
+    }
+}
--- Business-GUI/Vaughns-business/Vaughns-business/Classes/Staff.cs
+++ Business-GUI/Vaughns-business/Vaughns-business/Classes/Staff.cs
@@ -6,14 +6,17 @@ using System.Threading.Tasks;
 
 namespace Vaughns_business.Classes
 {
-    //public class Staff : Person
-    //{
-
-    //    public Staff(int id, string firstName, string lastName, string phoneNumber, string email)
-    //    : base(id, firstName, lastName)
-    //    {
-    //        this._phoneNum = phoneNumber;
-    //        this._custEmail = email;
-    //    }
-    //}
+    public class Staff : Person
+    {
+        private string _phoneNum;
+        private string _staffEmail;
+        public Staff(int id, string firstName, string lastName, string phoneNumber, string email)
+        : base(id, firstName, lastName)
+        {
+            this._phoneNum = phoneNumber;
+            this._staffEmail = email;
+        }
+        public string PhoneNumber { get => _phoneNum; set => _phoneNum = value; }
+        public string Email { get => _staffEmail; set => _staffEmail = value; }
+    }
 }
