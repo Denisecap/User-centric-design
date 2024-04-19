caadb083c1fa2d637527a374e673157d550dcf03
Elijah Hiraka <HIRAE1@student.op.ac.nz>
2024-04-19 21:19:22 +1200

    Added reading in staff from text file to Utils class

--- Business-GUI/Vaughns-business/Vaughns-business/Classes/Utils.cs
+++ Business-GUI/Vaughns-business/Vaughns-business/Classes/Utils.cs
@@ -22,9 +22,31 @@ namespace Vaughns_business.Classes
                 string lastName = customerDetails[2];
                 string phoneNum = customerDetails[3];
                 string email = customerDetails[4];
+                string order = customerDetails[5];
 
-                Customer customer = new Customer(id, firstName, lastName, phoneNum, email);
+                Customer customer = new Customer(id, firstName, lastName, phoneNum, email, order);
                 customers.Add(customer);
+            }
+        }
+
+        // reading in staff details
+        public static void ReadFromFile(string filePath, List<Staff> staffs)
+        {
+            List<string> lines = File.ReadAllLines(filePath).ToList();
+            foreach (string line in lines)
+            {
+                string[] staffDetails = line.Split(',');
+
+                int id = int.Parse(staffDetails[0]);
+                string firstName = staffDetails[1];
+                string lastName = staffDetails[2];
+                string phoneNum = staffDetails[3];
+                string email = staffDetails[4];
+                EPosition position = (EPosition)int.Parse(staffDetails[3]);
+                ESalary salary = (ESalary)int.Parse(staffDetails[4]);
+
+                Staff staff = new Staff(id, firstName, lastName, phoneNum, email, position, salary);
+                staffs.Add(staff);
             }
         }
     }
