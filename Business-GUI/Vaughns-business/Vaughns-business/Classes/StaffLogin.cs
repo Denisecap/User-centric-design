013f3174a9e8e97d7d803da27bd5ab36a3bb6143
Elijah Hiraka <HIRAE1@student.op.ac.nz>
2024-04-21 23:49:51 +1200

    added public fields for staff login class

--- Business-GUI/Vaughns-business/Vaughns-business/Classes/StaffLogin.cs
+++ Business-GUI/Vaughns-business/Vaughns-business/Classes/StaffLogin.cs
@@ -8,6 +8,17 @@ namespace Vaughns_business.Classes
 {
     public class StaffLogin
     {
-        private string _userId;
+        private int _userId;
+        private string _userName;
+        private string _passWord;
+        public StaffLogin(int userId, string userName, string passWord)
+        {
+            _userId = userId;
+            _userName = userName;
+            _passWord = passWord;
+        }
+        public int UserId { get => _userId; set => _userId = value; }
+        public string UserName { get => _userName; set => _userName = value; }
+        public string _PassWord { get => _passWord; set => _passWord = value; }
     }
 }
