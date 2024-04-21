37a94c8b78cb86df30ab8857f60be29d9f6e149b
Elijah Hiraka <HIRAE1@student.op.ac.nz>
2024-04-21 23:18:00 +1200

    adding a field to the orders table as customer id

--- Business-GUI/Vaughns-business/Vaughns-business/Classes/Order.cs
+++ Business-GUI/Vaughns-business/Vaughns-business/Classes/Order.cs
@@ -8,16 +8,19 @@ namespace Vaughns_business.Classes
 {
     public class Order
     {
-        private int _id;
+        private int _orderId;
+        private int _customerId;
         private string _name;
         private double _price;
-        public Order(int id, string name, double price)
+        public Order(int orderid, int custId, string name, double price)
         {
-            _id = id;
+            _orderId = orderid;
+            _customerId = custId;
             _name = name;
             _price = price;
         }
-        public int Id { get => _id; set => _id = value; }
+        public int OrderId { get => _orderId; set => _orderId = value; }
+        public int CustomerId { get => _customerId; set => _customerId = value; }
         public string Name { get => _name; set => _name = value; }
         public double Price { get => _price; set => _price = value; }
     }
